3b9db2c3180422b6af8bc91bcb86c9b7aa282aff
atifkodex <110590569+atifkodex@users.noreply.github.com>
2023-03-31 17:18:32 +0500

    update

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Intervention\Image\Facades\Image;
+
 
 
 class UserController extends Controller
@@ -48,23 +48,23 @@ class UserController extends Controller
         return $this->sendResponse([$test], 'Get data successfully');
     }
 
-    public function generatePng($text)
-    {
+    // public function generatePng($text)
+    // {
 
-        $img = Image::canvas(400, 200, '#ffffff');
-        $img->text($text, 200, 100, function ($font) {
-            $font->file(public_path('fonts/arial.ttf'));
-            $font->size(48);
-            $font->color('#000000');
-            $font->align('center');
-            $font->valign('middle');
-        });
-        $png = $img->encode('png');
-        return response($png, 200, [
-            'Content-Type' => 'image/png',
-            'Content-Disposition' => 'inline; filename="text.png"'
-        ]);
-    }
+    //     $img = Image::canvas(400, 200, '#ffffff');
+    //     $img->text($text, 200, 100, function ($font) {
+    //         $font->file(public_path('fonts/arial.ttf'));
+    //         $font->size(48);
+    //         $font->color('#000000');
+    //         $font->align('center');
+    //         $font->valign('middle');
+    //     });
+    //     $png = $img->encode('png');
+    //     return response($png, 200, [
+    //         'Content-Type' => 'image/png',
+    //         'Content-Disposition' => 'inline; filename="text.png"'
+    //     ]);
+    // }
     /////// create post.....///////
     public function addPost(Request $request)
     {
